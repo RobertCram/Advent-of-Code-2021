5dd6468625c9dbd581cd16efcd86f7333fc822c9
Robert Cram <memberships@xs4all.nl>
2021-12-02 10:34:07 +0000

    Prettifies all days overview.

--- alldays.jl
+++ alldays.jl
@@ -1,11 +1,20 @@
-println("Advent of Code 2021")
+function clearterminal()
+    print("\033c")
+end
+
+function bold(str)
+    "\033[1m$(str)\033[0m"
+end
+
+clearterminal()
+println()
+println(bold("Advent of Code 2021"))
 
 for i in 1:25
     filename = "day$(i)/day$(i).jl"
     !isfile(filename) && break
-    
-    println("")
+
+    println()
     println("Day $(i)")
     include("day$(i)/day$(i).jl")
 end
-

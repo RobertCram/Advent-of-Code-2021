3f3f28684d178541bcb44aea9686f4aec0b1c66c
Robert Cram <memberships@xs4all.nl>
2021-12-03 13:05:33 +0000

    Solves day 3.

--- day3/day3.jl
+++ day3/day3.jl
@@ -5,22 +5,52 @@ include("./../aoc.jl")
 using .AOC
 
 function AOC.processinput(data)
-    # data = split(data, '\n')
+    data = split(data, '\n')
+    data = [parse(Int, data[i][j]) for i in 1:length(data), j in 1:length(data[1])]
 end
 
-function solve1(input)
-    "nog te bepalen"
+function todecimal(binaryvector)
+    b = reverse([2^n for n in 0:length(binaryvector)-1])
+    (binaryvector * b)[1]
 end
 
-function solve2(input)
-    "nog te bepalen"
+function mostcommon(binarymatrix)
+    sum(binarymatrix, dims=1) .>= size(binarymatrix, 1) / 2
+end
+
+function leastcommon(binarymatrix)
+    .! mostcommon(binarymatrix)
+end
+
+function getrating(report, bitcriteria)
+    currentbit = 1
+    while size(report, 1) > 1
+        c = bitcriteria(report)
+        report = report[report[:, currentbit] .== c[currentbit], :]
+        currentbit += 1        
+    end
+    todecimal(report)
+end
+
+function solve1(report)
+    gammavector = mostcommon(report)
+    epsilonvector = .! gammavector
+    gamma = todecimal(gammavector)
+    epsilon = todecimal(epsilonvector)
+    gamma * epsilon
+end
+
+function solve2(report)
+    oxygenrating = getrating(report, mostcommon)
+    co2scrubberrating = getrating(report, leastcommon)
+    oxygenrating * co2scrubberrating
 end
 
 puzzles = [
-    Puzzle(3, "test 1", "input-test1.txt", solve1, nothing),
-    Puzzle(3, "deel 1", solve1, nothing),
-    Puzzle(3, "test 2", "input-test1.txt", solve2, nothing),
-    Puzzle(3, "deel 2", solve2, nothing)
+    Puzzle(3, "test 1", "input-test1.txt", solve1, 198),
+    Puzzle(3, "deel 1", solve1, 2648450),
+    Puzzle(3, "test 2", "input-test1.txt", solve2, 230),
+    Puzzle(3, "deel 2", solve2, 2845944)
 ]
 
 printresults(puzzles)
---
+++ day4/day4.jl
@@ -0,0 +1,28 @@
+module Day4
+
+include("./../aoc.jl")
+
+using .AOC
+
+function AOC.processinput(data)
+    # data = split(data, '\n')
+end
+
+function solve1(input)
+    "nog te bepalen"
+end
+
+function solve2(input)
+    "nog te bepalen"
+end
+
+puzzles = [
+    Puzzle(4, "test 1", "input-test1.txt", solve1, nothing),
+    Puzzle(4, "deel 1", solve1, nothing),
+    Puzzle(4, "test 2", "input-test1.txt", solve2, nothing),
+    Puzzle(4, "deel 2", solve2, nothing)
+]
+
+printresults(puzzles)
+
+end

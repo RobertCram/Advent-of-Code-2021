c09d050278c48af9fb0493fbc8ae891c9e01beeb
Robert Cram <memberships@xs4all.nl>
2021-12-10 11:07:20 +0000

    Solves day 10.

--- day10/day10.jl
+++ day10/day10.jl
@@ -5,22 +5,58 @@ include("./../aoc.jl")
 using .AOC
 
 function AOC.processinput(data)
-    # data = split(data, '\n')
+    data = split(data, '\n')
 end
 
-function solve1(input)
-    "nog te bepalen"
+const Openingbrackets = "([{<"
+const Closingbrackets = ")]}>"
+
+const ErrorScores = Dict(
+    ')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25137
+)
+
+const AutoCompleteScores = Dict(
+    '(' => 1,
+    '[' => 2,
+    '{' => 3,
+    '<' => 4
+)
+
+function getstack(line::AbstractString)    
+    stack = []
+    for c in line 
+        if c in Openingbrackets
+            push!(stack, c)
+        elseif c in Closingbrackets
+            pop!(stack) == Openingbrackets[findfirst(c, Closingbrackets)] || return (stack = stack, error = c)
+        end
+    end
+    (stack = stack, error = nothing)
 end
 
-function solve2(input)
-    "nog te bepalen"
+function getautocompletescore(stack)
+    reduce((score, _) -> 5 * score + AutoCompleteScores[pop!(stack)], 1:length(stack), init=0)
+end
+
+function solve1(lines)
+    errors = filter(e -> !isnothing(e), map(line -> getstack(line).error, lines))
+    sum(map(e -> ErrorScores[e], errors))
+end
+
+function solve2(lines)
+    lines = lines[map(line -> isnothing(getstack(line).error), lines)]
+    scores = sort(map(line -> getautocompletescore(getstack(line).stack), lines))
+    scores[(length(scores)+1) ÷ 2]
 end
 
 puzzles = [
-    Puzzle(10, "test 1", "input-test1.txt", solve1, nothing),
-    Puzzle(10, "deel 1", solve1, nothing),
-    Puzzle(10, "test 2", "input-test1.txt", solve2, nothing),
-    Puzzle(10, "deel 2", solve2, nothing)
+    Puzzle(10, "test 1", "input-test1.txt", solve1, 26397),
+    Puzzle(10, "deel 1", solve1, 319329),
+    Puzzle(10, "test 2", "input-test1.txt", solve2, 288957),
+    Puzzle(10, "deel 2", solve2, 3515583998)
 ]
 
 printresults(puzzles)
